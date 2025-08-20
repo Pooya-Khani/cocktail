47e567655d83d5cf4e49d382dbbcafc51b3ecab6
Pooya-Khani <pooyakhorshidkhani@gmail.com>
2025-08-20 23:56:00 +0330

    Art file animations

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,6 +1,36 @@
+import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
+import { useGSAP } from "@gsap/react";
 import { featureLists, goodLists } from "../../constants/index.js";
 
 const Art = () => {
+	const isMobile = useMediaQuery({ maxWidth: 767 });
+
+	useGSAP(() => {
+		const start = isMobile ? "top 20%" : "top top";
+
+		const maskTimeline = gsap.timeline({
+			scrollTrigger: {
+				trigger: "#art",
+				start,
+				end: "bottom center",
+				scrub: 1.5,
+				pin: true,
+			},
+		});
+
+		maskTimeline
+			.to(".will-fade", { opacity: 0, stagger: 0.2, ease: "power1.inOut" })
+			.to(".masked-img", {
+				scale: 1.3,
+				maskPosition: "center",
+				maskSize: "400%",
+				duration: 1,
+				ease: "power1.inOut",
+			})
+			.to("#masked-center", { opacity: 0, duration: 1, ease: "power1.inOut" });
+	});
+
 	return (
 		<div id="art">
 			<div className="container mx-auto h-full pt-20">
