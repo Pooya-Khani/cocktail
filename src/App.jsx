0d8c362a00d54ce3fa2fe8848f76dd10828ba4e7
Pooya-Khani <pooyakhorshidkhani@gmail.com>
2025-08-20 13:25:22 +0330

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { scrollTrigger, splitText } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(scrollTrigger, splitText);
+
+const App = () => {
+	return <div>App</div>;
+};
+
+export default App;
